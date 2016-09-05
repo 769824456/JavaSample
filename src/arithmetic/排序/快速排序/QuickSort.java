b58d18fea36991b5a90e447185a773e8c4ef1304
yulongsun007 <yulongsun0425@gmail.com>
2016-09-05 18:30:03 +0800

    更新 快排注释

--- src/arithmetic/排序/快速排序/QuickSort.java
+++ src/arithmetic/排序/快速排序/QuickSort.java
@@ -18,26 +18,36 @@ public class QuickSort {
 
     public static void quickSort(int[] list, int low, int high) {
         if (low < high) {
-            int middle = getMiddle(list, low, high);
+            int middle = partition(list, low, high);
+            //递归排序
             quickSort(list, low, middle - 1);
             quickSort(list, middle + 1, high);
         }
 
     }
 
-    private static int getMiddle(int[] list, int low, int high) {
-        int temp = list[low];
+    /**
+     * 分割
+     *
+     * @param list
+     * @param low
+     * @param high
+     * @return 获取枢轴位置
+     */
+    private static int partition(int[] list, int low, int high) {
+        int pivot = list[low];//枢轴位置
         while (low < high) {
-            while (low < high && list[high] >= temp) {
+            while (low < high && list[high] >= pivot) {//high指针往low走
                 high--;
             }
             list[low] = list[high];
-            while (low < high && list[low] <= temp) {
+            while (low < high && list[low] <= pivot) {//low指针往high走
                 low++;
             }
             list[high] = list[low];
         }
-        list[low] = temp;
+        //low==high就是循环停止的位置，也就是枢轴的位置
+        list[low] = pivot;
         return low;
     }
 }
