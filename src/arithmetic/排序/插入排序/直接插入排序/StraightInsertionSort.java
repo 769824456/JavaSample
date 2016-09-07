d164f94e7a80f56f5c37bcfbdab5c710917e0568
yulongsun007 <yulongsun0425@gmail.com>
2016-09-07 19:11:31 +0800

    更新 直接插入算法

--- src/arithmetic/排序/插入排序/直接插入排序/StraightInsertionSort.java
+++ src/arithmetic/排序/插入排序/直接插入排序/StraightInsertionSort.java
@@ -14,7 +14,7 @@ public class StraightInsertionSort {
 
     @Test
     public void testInsert() {
-        int[] arr    = {7, 1, 2, 3, 5, 4, 6, 8};
+        int[] arr = {7, 1, 2, 3, 5, 4, 6, 8};
         int[] result = straightInsertSort(arr);
 
         for (int x : result) {
@@ -23,11 +23,17 @@ public class StraightInsertionSort {
     }
 
 
-    /*直接插入排序*/
+    /**
+     * 直接插入排序
+     *
+     * 1.思路：a[0,a(i-1)]构成的子序列式排序的，将ai插入到子序列的适当位置，使得插入后的子序列仍是有序的。出入的位置有比较得到。
+     * 2.直接插入算法是稳定的；
+     *
+     */
     public int[] straightInsertSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int tmp = arr[i];
-            int j   = i - 1;
+            int j = i - 1;
             while ((j >= 0) && arr[j] > tmp) {
                 arr[j + 1] = arr[j];
                 j--;
@@ -36,5 +42,12 @@ public class StraightInsertionSort {
             arr[j + 1] = tmp;
         }
         return arr;
+    }
+
+
+    public void starghtInsert(int[] table) {
+        for (int i = 0; i < table.length; i++) {
+
+        }
     }
 }
